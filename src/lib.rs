6794bf9ee8e31565b4c61113958c3b41ab33f057
Kyllingene <fuzzymuffin343@gmail.com>
2023-10-19 21:13:53 -0700

    Fixed backtraces and location information

--- src/lib.rs
+++ src/lib.rs
@@ -17,22 +17,48 @@ pub fn catch(_attr: TokenStream, item: TokenStream) -> TokenStream {
 
     TokenStream::from(quote::quote! {
         #sig {
-            let old_hook = std::panic::take_hook();
-            std::panic::set_hook(Box::new(|_| {}));
+            let (__errata_tx, __errata_rx) = std::sync::mpsc::channel();
 
-            let result = std::panic::catch_unwind(|| {
+            let __errata_old_hook = std::panic::take_hook();
+            std::panic::set_hook(Box::new(move |info| {
+                let location = match info.location() {
+                    Some(l) => format!(" (at {}:{}:{})", l.file(), l.line(), l.column()),
+                    None => String::new(),
+                };
+
+                let _ = __errata_tx.send(location);
+            }));
+
+            let __errata_result = std::panic::catch_unwind(|| {
                 #block
             });
 
-            match result {
+            match __errata_result {
                 Ok(_) => return,
                 Err(e) => {
+                    let location = match __errata_rx.recv() {
+                        Ok(l) => l,
+                        Err(e) => {
+                            eprintln!("internal errata error (failed to recieve location): {e}");
+                            String::new()
+                        }
+                    };
+
                     if let Some(e) = e.downcast_ref::<errata::ErrataPanic>() {
                         eprintln!("{e}");
                     } else if let Some(e) = e.downcast_ref::<&str>() {
-                        eprintln!("error (at {}:{}:{}): {e}\n{}", file!(), line!(), column!(), std::backtrace::Backtrace::capture());
+                        eprintln!("error{location}: {e}");
+
+                        let bt = std::backtrace::Backtrace::capture();
+
+                        use std::backtrace::BacktraceStatus as BtS;
+                        match bt.status() {
+                            BtS::Captured => eprintln!("{bt}"),
+                            BtS::Disabled => eprintln!("run with `RUST_BACKTRACE=1` environment variable to display a backtrace"),
+                            _ => {}
+                        }
                     } else if let Some(e) = e.downcast_ref::<String>() {
-                        eprintln!("error (at {}:{}:{}): {e}", file!(), line!(), column!());
+                        eprintln!("error{location}: {e}");
 
                         let bt = std::backtrace::Backtrace::capture();
 
@@ -43,7 +69,15 @@ pub fn catch(_attr: TokenStream, item: TokenStream) -> TokenStream {
                             _ => {}
                         }
                     } else {
-                        eprintln!("Unhandled error in {} at line {}:{}\nErrored", file!(), line!(), column!());
+                        eprintln!("Unhandled error(location)");
+                        let bt = std::backtrace::Backtrace::capture();
+
+                        use std::backtrace::BacktraceStatus as BtS;
+                        match bt.status() {
+                            BtS::Captured => eprintln!("{bt}"),
+                            BtS::Disabled => eprintln!("run with `RUST_BACKTRACE=1` environment variable to display a backtrace"),
+                            _ => {}
+                        }
                     }
 
                     std::process::exit(1);
